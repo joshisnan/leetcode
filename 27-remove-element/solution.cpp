8b21c3baf04c14bc71f59fa67d5c67731c1cb9ae
joshisnan <templejo1@gmail.com>
2025-08-06 09:43:51 -0400

    add solution to 27-remove-element

---
+++ 27-remove-element/solution.cpp
@@ -0,0 +1,35 @@
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+// The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+// Consider the number of elements in nums which are not equal to val be k, to get accepted,
+// you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the elements which are not equal to val.
+// The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+#include<vector>
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+        for (int i = 0; i < nums.size(); ) {
+
+            if (nums[i] == val) {
+                nums.erase(nums.begin() + i);  // erase shifts elements left
+            } else {
+                ++i;  // only increment if we didn't erase
+            }
+        }
+
+        return nums.size();
+    }
+};
+
+int main() {
+    vector<int> nums = {3, 2, 2, 3};
+    int val = 2;
+    std::cout << Solution().removeElement(nums, val) << "\n";
+}
